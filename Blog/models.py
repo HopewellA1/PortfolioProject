631b07f0e945a24f13468ad653258b7de5d3d7fb
Yonela <hopewellsitshaka@gmail.com>
2022-07-25 09:50:22 +0200

    Commit with a blog basic code

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,11 @@
+from email.mime import image
+from pydoc import describe
+from pyexpat import model
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(max_length=1000)
+    date = models.DateField()
+    
--- MyPortfolio/urls.py
+++ MyPortfolio/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from Portfolio import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='Home')
+    path('',views.home,name='Home'),
+    path('Blog/', include('Blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
